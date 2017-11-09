0fa82cbc0894bdade0f8d115c990f5e21e803257
Le Hang <a1603195@myy.haaga-helia.fi>
2017-11-09 18:15:33 +0200

    first commit

---
+++ CosmeticShop/src/main/java/fi/hh/server/CosmeticShop/CosmeticShopApplication.java
@@ -0,0 +1,12 @@
+package fi.hh.server.CosmeticShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticShopApplication.class, args);
+	}
+}
---
+++ CosmeticShop/src/test/java/fi/hh/server/CosmeticShop/CosmeticShopApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.hh.server.CosmeticShop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CosmeticShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
